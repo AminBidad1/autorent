706599c4453d5d51c51460e58b35b18773bd28bd
ahmad.b <ahmadreza8287@gmail.com>
2024-12-18 03:40:18 +0330

    Add Post models, serializers and views

--- backend/srvs/office/office/views.py
+++ backend/srvs/office/office/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from backend.srvs.office.office.models import (
+    Post,
+    CarModel,
+)
+from backend.srvs.office.office.serializers import (
+    PostSerializer,
+    CarModelSerializer,
+)
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CarModelViewSet(ModelViewSet):
+    queryset = CarModel.objects.all()
+    serializer_class = CarModelSerializer
